12e751123be3ad169a881329b2694e9e35237e4f
Gru <energimen@yandex.ru>
2025-01-04 20:52:51 +0100

    Добавьте файлы проекта.

---
+++ SFmodul3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SFmodul3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Igor";
+            Console.WriteLine(MyName);
+            //Console.ReadKey();
+
+
+        }
+    }
+}
