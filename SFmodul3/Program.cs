613528f4ef7c094eef2249d0244fb1783e4ec1b8
Gru <energimen@yandex.ru>
2025-01-05 12:48:53 +0100

    Основные типы данных

--- SFmodul3/Program.cs
+++ SFmodul3/Program.cs
@@ -1,16 +1,43 @@
-﻿namespace SFmodul3
+﻿using System.Runtime;
+
+namespace SFmodul3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            const string MyName = "Igor";
-            Console.WriteLine(MyName);
-            
-            Console.WriteLine("\t Привет, мир");
-            Console.WriteLine("\t Мне 38 лет");
-            Console.WriteLine("\t My name is \n {0}", MyName);
-            Console.WriteLine('\u0023');
+            string name;
+            int age;
+            bool pet;
+            float foot_size;
+
+            Console.WriteLine("Как Вас зовут?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Сколько Вам лет?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Есть ли у Вас домашнее животное? да/нет");
+            string answer = Console.ReadLine();
+            if(answer.ToLower() == "да")
+            {
+                pet = true;
+            }
+            else
+            {
+                pet = false;
+            }
+
+            Console.WriteLine("Какой у Вас размер ноги?");
+            foot_size = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("YOUR PROFILE");
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("My age is {0}", age);
+            Console.WriteLine("Do I have a pet? {0}", pet);
+            Console.WriteLine("My shoe size is {0}", foot_size);
 
             //Console.ReadKey();
 
